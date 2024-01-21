947f1e49c05323caede30c23ee5f6e35a1bbe7c8
CoIorEvent8 <CoIorEvent8@gmail.com>
2024-01-21 18:12:05 -0500

    Completed the alert component.

---
+++ src/lib/components/Alert.tsx
@@ -0,0 +1,14 @@
+export function Alert(props) {
+  return (
+    <div className={`usa-alert usa-alert--${props.status || "info"}`}>
+      <div className="usa-alert__body">
+        <h4 className="usa-alert__heading">
+          {props.heading || "Untitled Alert"}
+        </h4>
+        <p className="usa-alert__text">
+          {props.children || "Lorem ipsum dolor sit amet."}
+        </p>
+      </div>
+    </div>
+  );
+}
