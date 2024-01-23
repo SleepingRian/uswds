7685a4461713609755923c295fcac2aaaa7ebae0
SleepingRian <thenewfrontierowner2021@gmail.com>
2024-01-22 19:28:05 -0500

    feat: updated alert component, by @colorevent

--- src/lib/components/Alert.tsx
+++ src/lib/components/Alert.tsx
@@ -1,12 +1,24 @@
+/*
+
+Props:
+  header: string
+  text: string
+  status: string (info, slim, emergency, heading, text, validation)
+
+*/
+
+
 export function Alert(props) {
   return (
     <div className={`usa-alert usa-alert--${props.status || "info"}`}>
       <div className="usa-alert__body">
         <h4 className="usa-alert__heading">
-          {props.heading || "Untitled Alert"}
+          {props.header || "Untitled Alert"}
         </h4>
         <p className="usa-alert__text">
-          {props.children || "Lorem ipsum dolor sit amet."}
+          {props.text}
+          {props.children}
+          {!props.text && !props.children && "No description provided."}
         </p>
       </div>
     </div>
